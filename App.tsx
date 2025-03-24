73131aa3f3e05a5c80e55993848fc8bd4b46e8e5
Corneille Tshibasu <89458796+c-tshibas@users.noreply.github.com>
2025-03-24 14:44:01 +0000

    [push] made changes to app and dependencies for repro

--- App.tsx
+++ App.tsx
@@ -6,152 +6,30 @@
  */
 
 import React from 'react';
-// import type {PropsWithChildren} from 'react';
-import {
-  // ScrollView,
-  // StatusBar,
-  // StyleSheet,
-  Text,
-  // useColorScheme,
-  View,
-} from 'react-native';
-
 import {
   BatchSize,
-  DdRum,
   SdkVerbosity,
   DatadogProvider,
   DatadogProviderConfiguration,
-  UploadFrequency
+  UploadFrequency,
+  TrackingConsent
 } from '@datadog/mobile-react-native';
 
-// import { DdRumReactNavigationTracking } from "@datadog/mobile-react-navigation";
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({children, title}: SectionProps): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// ORIGINAL CODE
-// function App(): React.JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   /*
-//    * To keep the template simple and small we're adding padding to prevent view
-//    * from rendering under the System UI.
-//    * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-//    * https://github.com/AppAndFlow/react-native-safe-area-context
-//    *
-//    * You can read more about it here:
-//    * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-//    */
-//   const safePadding = '5%';
-
-//   return (
-//     <View style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         style={backgroundStyle}>
-//         <View style={{paddingRight: safePadding}}>
-//           <Header/>
-//         </View>
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//             paddingHorizontal: safePadding,
-//             paddingBottom: safePadding,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </View>
-//   );
-// }
-// export default App;
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
+import { globalVariables } from './utils/variables' 
+import { WebView } from '@datadog/mobile-react-native-webview';
 
 // CONFIGURATION FOR THE DATADOG RN SDK
 const config = new DatadogProviderConfiguration(
-  "pub9d2ec44e0f94cf294aa949820cb07cc0",
-  "<ENV_NAME>",
-  "e4d2927c-efde-4d7e-bcce-8bd3f13bb9b8",
+  globalVariables.DD_CLIENT_TOKEN,
+  globalVariables.DD_ENV,
+  globalVariables.DD_RUM_APPLICATION_ID,
   true, // track User interactions (e.g.: Tap on buttons. You can use 'accessibilityLabel' element property to give tap action the name, otherwise element type will be reported)
   true, // track XHR Resources
-  true // track Errors
+  true, // track Errors
+  TrackingConsent.GRANTED
 )
 // Optional: Select your Datadog website (one of "US1", "EU1", "US3", "US5", "AP1" or "GOV")
-config.site = "US1";
+config.site = 'US1';
 // Optional: Enable JavaScript long task collection
 config.longTaskThresholdMs = 100;
 // Optional: enable or disable native crash reports
@@ -169,17 +47,10 @@ config.verbosity = SdkVerbosity.DEBUG;
 export default function HelloWorldApp() {
   return (
     <DatadogProvider configuration={config}>
-            {/* <Navigation /> */}
-      <View
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        onLayout={() => {DdRum.startView('Home-Screen', 'Home', {'custom.foo': 'Home'}, Date.now())}}>
-        <Text>Hello, world!</Text>
-      </View>
+      <WebView
+        source={{ uri: 'https://qtf8f655301d564bb4b0139f539f.free.beeceptor.com/' }}
+        allowedHosts={['qtf8f655301d564bb4b0139f539f.free.beeceptor.com']}
+      />
     </DatadogProvider>
   );
 };
